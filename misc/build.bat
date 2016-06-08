c8f516729a2f0b01bd5d63186b462fda5fb64af2
Saticmotion <simon_verbeke_@hotmail.com>
2016-06-08 21:46:11 +0200

    Changed build system a bit to work better with Sublime

--- misc/build.bat
+++ misc/build.bat
@@ -1,12 +1,15 @@
+@echo Debug build
+@echo.
+
 @echo off
 
-set VARS=/Od /Zi /W4 /wd4130 /wd4996
+set VARS=/Od /Zi /FC /W4 /wd4130 /wd4996 /nologo
 
 set files=..\code\sv_markdown_test.c
 
 set options=%VARS% %files%
 
-mkdir ..\build
+mkdir ..\build > nul 2> nul
 
 pushd ..\build
 
--- misc/build_optimized.bat
+++ misc/build_optimized.bat
@@ -1,12 +1,14 @@
+@echo Optimized build
+@echo.
 @echo off
 
-set VARS=/O2 /Zi /Oi /W4 /wd4996
+set VARS=/O2 /Zi /Oi /FC /W4 /wd4996 /nologo
 
 set files=..\code\sv_markdown_test.c
 
 set options=%VARS% %files%
 
-mkdir ..\build
+mkdir ..\build > nul 2> nul
 
 pushd ..\build
 
